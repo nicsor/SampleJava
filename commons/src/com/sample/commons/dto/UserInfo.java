786db5bd2ae477403623b6cb72a67ddeb69e21ed
Nicolae Natea <roscin@gmail.com>
2019-12-23 12:51:52 +0200

    Create initial data transfer objects
    Signed-off-by: Nicolae Natea <roscin@gmail.com>

---
+++ commons/src/com/sample/commons/dto/UserInfo.java
@@ -0,0 +1,33 @@
+package com.sample.commons.dto;
+
+import java.util.List;
+
+public class UserInfo {
+	String username;
+	String name;
+	List<String> projects;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<String> projects) {
+		this.projects = projects;
+	}
+}
